c540ddaddee635faefa89a7b3f4c7d24a16c43fd
Roger Chau <RogerChau1999@gmail.com>
2022-10-31 17:14:57 -0400

    models/enums created

---
+++ src/main/java/com/revature/models/Users.java
@@ -0,0 +1,110 @@
+package com.revature.models;
+
+import com.revature.utils.Role;
+
+import java.util.Objects;
+
+public class Users {
+    private int user_id;
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private Role role;
+
+    public Users(int user_id, String username, String password, String first_name, String last_name, String email, Role role) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Users() {
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return user_id == users.user_id && username.equals(users.username) && password.equals(users.password) && first_name.equals(users.first_name) && last_name.equals(users.last_name) && email.equals(users.email) && role == users.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_id, username, password, first_name, last_name, email, role);
+    }
+}
---
+++ src/main/java/com/revature/utils/Material.java
@@ -0,0 +1,5 @@
+package com.revature.utils;
+
+public enum Material {
+    IRON, GOLD, DIAMOND, NETHERITE
+}
