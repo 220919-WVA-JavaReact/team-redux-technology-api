892996d294d6a8289e199642548c128c475450fc
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-10-31 17:42:00 -0400

    fixed main import

--- src/main/java/com/revature/MinecraftStoreApplication.java
+++ src/main/java/com/revature/MinecraftStoreApplication.java
@@ -1,4 +1,4 @@
-package com.revature.minecraftstore;
+package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
