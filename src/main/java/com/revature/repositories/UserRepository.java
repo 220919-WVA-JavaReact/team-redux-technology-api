d3f6f8afd8d7d6fa9e46dce7020dccd7b59f0aff
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-10-31 16:09:35 -0400

    adding userrepo constructor

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -1,7 +1,16 @@
 package com.revature.repositories;
 
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
+
+    private UserService us;
+
+    @Autowired
+    public UserRepository(UserService us) {
+        this.us = us;
+    }
 }
