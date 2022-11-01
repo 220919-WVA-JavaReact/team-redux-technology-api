4fc878c25511b7892da2d1f25631373de82bec88
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-11-01 11:12:27 -0400

    fixed deps?

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -1,16 +1,12 @@
 package com.revature.repositories;
 
-import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
 
-    private UserService us;
-
     @Autowired
-    public UserRepository(UserService us) {
-        this.us = us;
+    public UserRepository() {
     }
 }
