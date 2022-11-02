a857410ee9679e92bbd9997bc3ecd162302358a0
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-11-02 14:16:18 -0400

    imported mvc

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,4 @@
-insert into users (user_id, username, password, email, role) values ('1', 'jonny', 'pass', 'bow682@gmail.com', 'BUYER');
+insert into users (user_id, username, password, email, role) values ('a', 'jonny', 'pass', 'bow682@gmail.com', 'BUYER');
+insert into users (user_id, username, password, email, role) values ('b', 'lesly', 'pass', 'lesly@email.com',  'ADMIN');
+insert into users (user_id, username, password, email, role) values ('c', 'roger', 'pass', 'roger@email.com',  'ADMIN');
+insert into users (user_id, username, password, email, role) values ('d', 'lily',  'pass', 'lily@email.com',   'ADMIN');
