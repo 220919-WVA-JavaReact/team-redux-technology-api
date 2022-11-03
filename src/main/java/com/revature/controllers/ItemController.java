889fcec4e8a7e076b30eb4ae2acee2953c9c3e0a
Lesly <leslyr562@gmail.com>
2022-11-03 16:00:19 -0500

    Item by Id

--- src/main/java/com/revature/controllers/ItemController.java
+++ src/main/java/com/revature/controllers/ItemController.java
@@ -4,15 +4,17 @@ import com.revature.entities.Item;
 import com.revature.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/items")
 public class ItemController {
 
     private ItemService is;
@@ -22,8 +24,21 @@ public class ItemController {
         this.is = is;
     }
 
-    @GetMapping("/items")
+    @GetMapping
     public ResponseEntity<List<Item>> getItems(){
         return new ResponseEntity<>(is.getAllItems(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    //Item by ID Method with ID param
+    public ResponseEntity<Item> getById(@PathVariable("id") String id) {
+        Optional<Item> items = is.getItemById(id);
+        if(items.isPresent()){
+            return new ResponseEntity<>(items.get() , HttpStatus.OK);
+        }else{
+            throw new NullPointerException();
+        }
+
+    }
+
 }
