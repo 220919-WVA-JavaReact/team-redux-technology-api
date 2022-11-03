3a3ea423967176983846ad776efaf43615509644
Roger Chau <RogerChau1999@gmail.com>
2022-11-03 17:18:27 -0400

    authcontroller, exceptions created, services and repositories edited

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -4,8 +4,10 @@ import com.revature.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    User findByUsername(String username);
+    Optional<User> findUserByUsernameAndPassword(String username, String password);
 }
